398740fff4e526bb1b1216221d7a1ac18da9c6b4
Tejaswi Gowda <tejaswigowda@macbookpro.dhcp.asu.edu>
2026-01-28 17:52:35 -0700

    ac

--- basicServers/staticHosting/server.js
+++ basicServers/staticHosting/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var server = express();
 var bodyParser = require('body-parser');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 1234;
+var port = 8080;
 
 server.get("/", function (req, res) {
     res.redirect("index.html")
